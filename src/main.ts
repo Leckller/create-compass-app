8049d388ac5d85d3a56a3b4a60d49c87463604cc
Gusttavo Ruy <7leckler@gmail.com>
2024-07-10 17:20:57 -0300

    feat: options

--- src/main.ts
+++ src/main.ts
@@ -5,26 +5,46 @@ import { spawn } from "child_process";
 import path from "path";
 import fs from "fs";
 import { stdout } from "process";
-import { input } from "@inquirer/prompts";
+import { input, select } from "@inquirer/prompts";
+
+type frameworks = "React" | "Default";
+type styles = "Tailwind" | "Styled-Components" | "Default";
+
+interface OptionsProject {
+  framework: frameworks;
+  style: styles;
+}
 
 class main {
   private _projectName = "";
+  private _options: OptionsProject = {
+    framework: "Default",
+    style: "Default",
+  };
   private _projectPath = "";
 
   constructor() {
-    this.projectName();
+    this.start();
   }
 
-  public async projectName() {
-    // Pede o nome do projeto
-    const projectName = await input({
-      message: "Insira um nome para o projeto:",
-    });
-    this._projectName = projectName;
+  protected async start() {
+    await this.projectName();
     this.projectPath();
+    await this.projectOptions();
+    console.log(this);
+    // const makeDirProj = spawn("mkdir", [this._projectName]);
   }
 
-  public projectPath() {
+  private async projectName() {
+    // Pede o nome do projeto
+    const projectName =
+      (await input({
+        message: "Insira um nome para o projeto:",
+      })) || "project-uol-compass";
+    this._projectName = projectName;
+  }
+
+  private async projectPath() {
     // cria o caminho para o diretorio do projeto
     const projectPath = path.join(process.cwd(), this._projectName);
     //verifica se já existe um diretorio com esse nome.
@@ -35,7 +55,43 @@ class main {
       process.exit(1);
     }
     this._projectPath = projectPath;
-    const makeDirProj = spawn("mkdir", [this._projectName]);
+  }
+
+  private async projectOptions() {
+    const framework: frameworks = await select({
+      message: "Qual framework você quer usar?",
+      theme: {
+        icon: { cursor: "💙" },
+      },
+      choices: [
+        {
+          name: "React",
+          value: "React",
+        },
+        {
+          name: "Default",
+          value: "Default",
+        },
+      ],
+    });
+    const style: styles = await select({
+      message: "Qual tipo de ",
+      choices: [
+        {
+          name: "Tailwind",
+          value: "Tailwind",
+        },
+        {
+          name: "Styled-Components",
+          value: "Styled-Components",
+        },
+        {
+          name: "Default",
+          value: "Default",
+        },
+      ],
+    });
+    this._options = { style, framework };
   }
 }
 
